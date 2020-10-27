73a8aeeee4f6beb8508698efbbe4a4dfd5a4c533
Ahmed Kortas <42817639+ahmedkortas@users.noreply.github.com>
2020-10-27 10:46:04 +0100

    conflic resolved bassma work merged

--- client/index.jsx
+++ client/index.jsx
@@ -1,58 +1,34 @@
 import React from "react";
 import ReactDom from "react-dom";
 import Home from "./component/Home.jsx";
-import PostAdd from "./component/PostAdd/PostAdd.jsx";
-import Adds from "./component/adds/Adds.jsx";
-import LogIn from "./component/login/Login.jsx";
 import "./style.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      view: "home",
-    };
-    this.onClick = this.onClick.bind(this);
   }
-
-  onClick(e) {
-    this.setState({ view: e.target.innerHTML });
-  }
-
   render() {
     return (
       <div>
+        <Home />
         <div className="header">
           <ul>
             <li>
-              <a onClick={this.onClick}>Home</a>
+              <a>Home</a>
             </li>
             <li>
-              <a onClick={this.onClick}>PostAdd</a>
+              <a>Put</a>
             </li>
             <li>
-              <a onClick={this.onClick}>Adds</a>
+              <a>Adds</a>
             </li>
             <li>
-              <a onClick={this.onClick}>Your offers</a>
+              <a>Your offers</a>
             </li>
             <li>
-              <a onClick={this.onClick}>Login</a>
+              <a>Login</a>
             </li>
           </ul>
-        </div>
-        <div>
-          {this.state.view === "Home" ? (
-            <Home />
-          ) : this.state.view === "PostAdd" ? (
-            <PostAdd />
-          ) : this.state.view === "Adds" ? (
-            <Adds />
-          ) : this.state.view === "Login" ? (
-            <LogIn />
-          ) : (
-            <div></div>
-          )}
         </div>
       </div>
     );
