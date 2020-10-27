f52d2ad012f438ccdc3d13028c439396c71e1e3e
Ahmed Kortas <42817639+ahmedkortas@users.noreply.github.com>
2020-10-27 13:12:05 +0100

    working on the querry

--- client/component/adds/Adds.jsx
+++ client/component/adds/Adds.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import axios from "axios";
 
 class Adds extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      data: [],
+    };
+    this.querry = this.querry.bind(this);
+  }
+
+  querry() {
+    axios.get("/api/Ads").then((response) => {
+      if (this.state.data.length !== response.length) {
+        console.log(response);
+        // this.setState({ data: response });
+      }
+    });
   }
 
   render() {
+    this.querry();
     return <div></div>;
   }
 }
--- server/index.js
+++ server/index.js
@@ -2,10 +2,16 @@ const express = require("express");
 let PORT = 3000;
 const bodyParser = require("body-parser");
 let app = express();
+let dataQueue = require("./dataQuerry.js");
 
 app.use(express.static(__dirname + "/../public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api/Ads", (req, res) => {
+  console.log(req);
+  res.send("hey");
+});
 
 app.listen(PORT, () => {
   console.log("server is connected");
