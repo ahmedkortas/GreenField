a91ec726d5b34c3aad66c24de622e079d6379167
MohamedAmineDerouiche <mohderouiche250881@gmail.com>
2020-10-27 13:49:45 +0100

    creat server for SingUp

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 let PORT = 3000;
-// let user = require("../db/user.js")
+
 const bodyParser = require("body-parser");
 let app = express();
 
@@ -8,12 +8,14 @@ app.use(express.static(__dirname + "/../public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.post("/api/user", (req, res) => {
-//   let obj = req.body;
-//   user(obj).then((user) => {
-//     res.send(str);
-//   });
-// });
+
+app.post("/api/SignUp", function (req, res) {
+  let obj = req.body;
+  SignUp.create(obj).then((data) => {
+    str = JSON.stringify(data);
+    res.send(data);
+  });
+});
 
 
 app.listen(PORT, () => {
