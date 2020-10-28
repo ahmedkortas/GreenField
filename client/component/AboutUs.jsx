2ef820ad40ab4c429c948042a8c40af75cf81009
Ahmed Kortas <42817639+ahmedkortas@users.noreply.github.com>
2020-10-28 11:10:29 +0100

    changes approved

--- client/component/AboutUs.jsx
+++ client/component/AboutUs.jsx
@@ -1,30 +1,28 @@
 //this page when you click on the button on the home page "more information it will be render it"
 
-import React from 'react';
-import "./style/Component/About.css"
+import React from "react";
+import "./style/Component/About.css";
 
 class AboutUs extends React.Component {
-    constructor(props) {
-      super(props);
-    }
-    render() {
-      return (
-        <div>
-       <div className="about">
-           <h1>About Us...</h1>
-           <p>helloooooooooo
-               how arrrrrre you
-               this is just an e.g of text 
-               to teeeesssst the component
-           </p>
-           <button className="btn">Read More<i className="fas fa-book-open"></i></button>
-       </div>
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <div className="about">
+          <h1>About Us...</h1>
+          <p>
+            helloooooooooo how arrrrrre you this is just an e.g of text to
+            teeeesssst the component
+          </p>
+          <button className="btn">
+            Read More<i className="fas fa-book-open"></i>
+          </button>
+        </div>
       </div>
-      )
-     
-      
-    }
+    );
+  }
 }
- 
-export default AboutUs;
 
+export default AboutUs;
