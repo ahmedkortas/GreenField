d03ed4af36c25012cfa32c6df8c23e48eccc4fc5
MohamedAmineDerouiche <mohderouiche250881@gmail.com>
2020-10-28 11:57:13 +0100

    post registration ok to data

--- server/index.js
+++ server/index.js
@@ -17,17 +17,30 @@ app.get("/api/Ads", (req, res) => {
 
 app.post("/api/SignUp", function (req, res) {
   console.log(req.body);
+
+
   findByEmail(req.body.email)
     .then(data => {
      
+      if(data.length>0)
+        return res.status(400).send("email already exist");
       
-      create(req.body).then().catch();
+      
+      create(req.body).then(data=>{
+        res.json(data)
+      }).catch(err=>{
+        console.log(err)
+        res.status(400).send("some information missing")
+      });
     })
-    .catch();
+    .catch(err=>{
+      console.log(err)
+      res.status(500).send("server error")
+    });
 
 
   // user(req.body).save().then(g =>console.log(g))
-  res.send("hi");
+ 
   // let obj = req.body;
   // SignUp.create(obj).then((data) => {
   //   str = JSON.stringify(data);
