295643b20ada53fa6439a183e1daa86c3061113f
HamdiBenkahla <68249436+HamdiBenkahla@users.noreply.github.com>
2020-10-29 20:54:24 +0100

    css on the adds

--- client/component/adds/SmallJobA.jsx
+++ client/component/adds/SmallJobA.jsx
@@ -17,11 +17,11 @@ class SmallJobA extends React.Component {
     <div className="feed">
     <ul>
       <li className="feed-list-item">
-        <span className="feed-list-item-title">{this.props.data.title}</span><br></br>        
-        <p className="feed-list-item-description">{this.props.data.description}</p> <br></br><br></br>     
+        <span className="feed-list-item-title">{this.props.data.title}</span><br></br><br></br>         
+        <p className="feed-list-item-description">{this.props.data.description}</p> <br></br>     
         <span  className="feed-list-item-poster">{this.props.data.contact}</span><br></br><br></br>
-        <span  className="feed-list-item-price">{this.props.data.price}DT</span><br></br><br></br>
-        <span  className="feed-list-item-poster">{this.props.data.address}</span> <br></br><br></br> 
+        <span  className="feed-list-item-price"><strong> Earning: </strong>{this.props.data.price}DT</span><br></br><br></br>
+        <span  className="feed-list-item-address"><strong>  Location : </strong> {this.props.data.address}</span> <br></br><br></br> 
         <span><button  type="button" className="btn-primary">Apply here </button></span>
       </li>
       </ul>    
